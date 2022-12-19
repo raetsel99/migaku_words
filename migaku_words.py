29b1ee5a386f0c66d0ee418e685365a083c11ca8
Simon Stanford <simon.stanford@eclipsegroup.co.uk>
2022-12-19 12:25:27 +0000

    Working version

--- migaku_words.py
+++ migaku_words.py
@@ -3,6 +3,7 @@ import csv
 import os
 import argparse
 from pathlib import Path
+import sys
 
 # quick and dirty script to update the words being learned in missingPackagesAndExecutables
 
@@ -11,8 +12,14 @@ def read_json_in(file_in):
     return json.loads(fileIn.read())
 
 def import_csv(json_array, file_in, word_status):
-    dummy_data=["test1",2]
-    json_array.append(dummy_data)
+    # dummy_data=["test1",2]
+    
+    with open(file_in, newline='') as csvfile:
+        word_reader = csv.reader(csvfile)
+        for row in word_reader:
+            row.append(word_status)
+            json_array.append(row)
+     
     return json_array
 
 
@@ -30,25 +37,37 @@ if __name__ == '__main__':
     
     # get the input file if present
     if args.json_input:
-        agnostic_jason_in = Path(args.json_input)
-        if os.path.exists(agnostic_jason_in):
-            migaku_array = read_json_in(agnostic_jason_in)
+        agnostic_json_in = Path(args.json_input)
+        if os.path.exists(agnostic_json_in):
+            migaku_array = read_json_in(agnostic_json_in)
             print(f"json is {migaku_array}")
         else:
-            print(f"ERROR E001 could not find file {agnostic_jason_in}")
+            print(f"ERROR E001 could not find file {agnostic_json_in}")
+            sys.exit(1)
 
     if args.status and (args.status <1 or args.status >2):
         print(f"ERROR E002 invalid status {args.status}. 1=learning, 2=known")
+        sys.exit(2)
     else:
         word_status = args.status
 
  
 
     agnostic_csv_in = Path(args.csv_file)
-    migaku_array = import_csv(migaku_array,agnostic_csv_in,word_status)
+    if os.path.exists(agnostic_csv_in):
+        migaku_array = import_csv(migaku_array,agnostic_csv_in,word_status)
+    else:
+        print(f"ERROR E003 could not find file {agnostic_csv_in}")
+        sys.exit(3)
+
     print(f"migaku array is now: {migaku_array}")
 
     agnostic_json_out=Path(args.json_output)
+    with open(agnostic_json_out, "w") as write_file:
+        json.dump(migaku_array, write_file)
+
+    print(f"Data written to {agnostic_json_out}. Be sure to backup Migaku before importing")
+
 
 
 
